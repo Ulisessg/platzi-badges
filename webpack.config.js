ab342bb1f78e6b30215f0934231286110f03dd6f
UlisesSG <ukisessg@gmail.com>
2020-08-11 15:54:21 -0500

    Commit de prueba

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 2267,
+    publicPath: 'http://localhost:2267/dist/',
+    hotOnly: true,
+    watchContentBase: true,
+    watchOptions: {
+      poll: true,
+    },
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
