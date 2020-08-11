7f1d4c1193b0073504e52dfb1a782bfd09574b62
UlisesSG <ukisessg@gmail.com>
2020-08-11 17:27:40 -0500

    Estilos añadidos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+
 import Logo from '../../images/logo.svg';
 import Profile from '../../images/profile.png';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/Badge.css';
+import '../global.css';
 
 class Badge extends Component {
   render() {
     return (
-      <div className='Badge'>
-        <div>
-          <img src={Logo} alt='Logo de la conferencia' />
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={Logo} alt="Logo de la conferencia" />
         </div>
-        <div>
+
+        <div className="Badge__section--name">
+          <img className="Badge__avatar" src={Profile} alt="Avatar" />
           <h1>
             Ulises
             <br />
-            SG
+            Sámano
           </h1>
         </div>
-        <div>
-          <img src={Profile} alt='Avatar' />
-          <p>Frontend-Developer</p>
-          <p>@Ulises5G</p>
+
+        <div className="Badge__section--info">
+          <h3>Frontend-Developer</h3>
+          <div>@Ulises5G</div>
         </div>
-        <div>
+
+        <div className="Badge__footer">
           <p>#PlatziConf</p>
         </div>
       </div>
